cbcec01aaa5befc8202cfd411442eb6d9fe5ce1b
baothi <baothi246@gmail.com>
2021-12-12 12:30:22 +0700

    add create carts

--- store/views.py
+++ store/views.py
@@ -3,7 +3,10 @@ from store.models import Product#, ReviewRating
 from category.models import Category
 from django.http import HttpResponse, JsonResponse,HttpResponseRedirect
 import json
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
+from carts.models import CartItem
+from carts.views import _cart_id
 # Create your views here.
 
 def store(request, category_slug=None):
@@ -13,18 +16,18 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
-        # paginator = Paginator(products, 1)
-        # page = request.GET.get('page')
-        # paged_products = paginator.get_page(page)
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True).order_by('id')
-        # paginator = Paginator(products, 3)
-        # page = request.GET.get('page')
-        # paged_products = paginator.get_page(page)
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count' : product_count,
     }
     return render(request, "store/store.html", context)
@@ -45,7 +48,7 @@ def get_product_detail(request):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        # in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
@@ -65,7 +68,7 @@ def product_detail(request, category_slug, product_slug):
 
     context = {
         'single_product': single_product,
-        # 'in_cart'       : in_cart,
+        'in_cart'       : in_cart,
         # 'orderproduct': orderproduct,
         # 'reviews': reviews,
         # 'product_gallery': product_gallery,
