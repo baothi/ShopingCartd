581a4b0bc59bb7d5e14582e6e2050b261f77a28a
baothi <baothi246@gmail.com>
2022-02-23 15:48:47 +0700

    finish dashbroad

--- accounts/views.py
+++ accounts/views.py
@@ -35,10 +35,10 @@ def register(request):
             user.save()
 
             # Create a user profile
-            # profile = UserProfile()
-            # profile.user_id = user.id
-            # profile.profile_picture = 'default/default-user.png'
-            # profile.save()
+            profile = UserProfile()
+            profile.user_id = user.id
+            profile.profile_picture = 'default/default-user.png'
+            profile.save()
 
             # USER ACTIVATION
             # breakpoint()
@@ -157,10 +157,10 @@ def dashboard(request):
     orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
     orders_count = orders.count()
 
-    # userprofile = UserProfile.objects.get(user_id=request.user.id)
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
     context = {
         'orders_count': orders_count,
-        # 'userprofile': userprofile,
+        'userprofile': userprofile,
     }
     return render(request, 'accounts/dashboard.html', context)
 
@@ -238,14 +238,10 @@ def my_orders(request):
 
 @login_required(login_url='login')
 def edit_profile(request):
-    print("===================request.POST ================= : ", request.POST)
     userprofile = get_object_or_404(UserProfile, user=request.user)
-    print("===================userprofile================= : ", userprofile)
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
-        print("===================user_form ================= : ", user_form)
         profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
-        print("===================profile_form ================= : ", profile_form)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
@@ -276,3 +272,32 @@ def order_detail(request, order_id):
         'subtotal': subtotal,
     }
     return render(request, 'accounts/order_detail.html', context)
+
+
+@login_required(login_url='login')
+def change_password(request):
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+
+        user = Account.objects.get(username__exact=request.user.username)
+
+        if new_password == confirm_password:
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+                # auth.logout(request)
+                messages.success(request, 'Password updated successfully.')
+                return redirect('change_password')
+            else:
+                messages.error(request, 'Please enter valid current password')
+                return redirect('change_password')
+        else:
+            messages.error(request, 'Password does not match!')
+            return redirect('change_password')
+    return render(request, 'accounts/change_password.html')
+
+
+
